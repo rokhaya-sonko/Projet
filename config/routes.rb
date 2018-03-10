ba1f4e2635677cbc8cdaed0f012b955f675cebec
rokhaya.sonko <rokhaya.sonko@gmail.com>
2018-03-10 05:15:33 -0500

    New commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :students
+  resources :posts
+  resources :posts
+  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
