0e5a1e6e70567c1385cd15c439204fc4c6099346
rokhaya.sonko <rokhaya.sonko@gmail.com>
2018-03-16 17:00:39 -0400

    Students

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,7 @@ ActiveRecord::Schema.define(version: 20180310101013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+  
   create_table "students", force: :cascade do |t|
     t.string   "name"
     t.string   "lastname"
